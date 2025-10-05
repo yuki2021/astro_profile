a5f7b65c9f898bd07e48f2d661fb03fdc8b279d8
yuki2021 <ordinary.plusminus1@gmail.com>
2025-10-05 21:32:20 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yuki2021.github.io',
 	integrations: [mdx(), sitemap()],
 });
