8b5a8f74826fe612c580d623d47dc0e8c92dafe8
yuki2021 <ordinary.plusminus1@gmail.com>
2025-10-06 08:32:57 +0900

    表示されないので修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yuki2021.github.io',
-
+	base: '/profile_astro',
 	vite: {
 		plugins: [tailwindcss()],
 	},
